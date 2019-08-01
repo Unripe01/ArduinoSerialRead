841b203b207b036cb643f64acfcd816a2aa768f6
Unripe01 <dalmore.name@gmail.com>
2019-08-01 16:51:05 +0900

    初回コミット

--- Arduino/Form1.Designer.cs
+++ Arduino/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            this.comboBox_COM = new System.Windows.Forms.ComboBox();
+            this.btnHigh = new System.Windows.Forms.Button();
+            this.btnLow = new System.Windows.Forms.Button();
+            this.comboBox_RATE = new System.Windows.Forms.ComboBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox_COM
+            // 
+            this.comboBox_COM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_COM.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox_COM.FormattingEnabled = true;
+            this.comboBox_COM.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5"});
+            this.comboBox_COM.Location = new System.Drawing.Point(34, 60);
+            this.comboBox_COM.Name = "comboBox_COM";
+            this.comboBox_COM.Size = new System.Drawing.Size(157, 39);
+            this.comboBox_COM.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            label1.Location = new System.Drawing.Point(28, 26);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(124, 31);
+            label1.TabIndex = 1;
+            label1.Text = "COMポート";
+            // 
+            // btnHigh
+            // 
+            this.btnHigh.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnHigh.Location = new System.Drawing.Point(31, 149);
+            this.btnHigh.Name = "btnHigh";
+            this.btnHigh.Size = new System.Drawing.Size(157, 39);
+            this.btnHigh.TabIndex = 2;
+            this.btnHigh.Text = "High";
+            this.btnHigh.UseVisualStyleBackColor = true;
+            this.btnHigh.Click += new System.EventHandler(this.btnHigh_Click);
+            // 
+            // btnLow
+            // 
+            this.btnLow.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnLow.Location = new System.Drawing.Point(220, 149);
+            this.btnLow.Name = "btnLow";
+            this.btnLow.Size = new System.Drawing.Size(157, 39);
+            this.btnLow.TabIndex = 2;
+            this.btnLow.Text = "Low";
+            this.btnLow.UseVisualStyleBackColor = true;
+            this.btnLow.Click += new System.EventHandler(this.btnLow_Click);
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            label2.Location = new System.Drawing.Point(25, 115);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(166, 31);
+            label2.TabIndex = 1;
+            label2.Text = "13番ピンの制御";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            label3.Location = new System.Drawing.Point(214, 26);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(197, 31);
+            label3.TabIndex = 1;
+            label3.Text = "転送レート（bps）";
+            // 
+            // comboBox_RATE
+            // 
+            this.comboBox_RATE.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_RATE.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox_RATE.FormattingEnabled = true;
+            this.comboBox_RATE.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200"});
+            this.comboBox_RATE.Location = new System.Drawing.Point(220, 60);
+            this.comboBox_RATE.Name = "comboBox_RATE";
+            this.comboBox_RATE.Size = new System.Drawing.Size(157, 39);
+            this.comboBox_RATE.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(547, 302);
+            this.Controls.Add(this.btnLow);
+            this.Controls.Add(this.btnHigh);
+            this.Controls.Add(label2);
+            this.Controls.Add(label3);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.comboBox_RATE);
+            this.Controls.Add(this.comboBox_COM);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Arduino Uno";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_COM;
+        private System.Windows.Forms.Button btnHigh;
+        private System.Windows.Forms.Button btnLow;
+        private System.Windows.Forms.ComboBox comboBox_RATE;
     }
 }
 
--- Arduino/Form1.cs
+++ Arduino/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Arduino
 {
     public partial class Form1 : Form
     {
+        private enum Digital
+        {
+            h,l
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox_COM.SelectedItem = "COM3";
+            comboBox_RATE.SelectedItem = "9600";
+        }
+
+        private void btnHigh_Click(object sender, EventArgs e)
+        {
+            WriteToCom(Digital.h);
+        }
+
+        private void btnLow_Click(object sender, EventArgs e)
+        {
+            WriteToCom(Digital.l);
+        }
+
+        private void WriteToCom(Digital button)
+        {
+            SerialPort mySerialPort = new SerialPort(comboBox_COM.SelectedItem.ToString());
+            mySerialPort.BaudRate = int.Parse(comboBox_RATE.SelectedItem.ToString());
+            mySerialPort.Open();
+            mySerialPort.Write(button.ToString());
+            mySerialPort.Close();
+        }
+
     }
 }
